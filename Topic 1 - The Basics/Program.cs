aaad59277c0ab749cdefa45a7f076c34f32c63b5
339729139 <339729139@2260I-L29-009>
2024-09-05 09:29:22 -0400

    Add project files.

---
+++ Topic 1 - The Basics/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Topic_1___The_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sam Ahrens September 5th 2024
+            /*the code below will print the words "Hello World"
+            to the screen, and it is amazing*/
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Press Enter to Continue");
+            Console.ReadLine();
+            Console.WriteLine("This is on a new line.");
+            Console.WriteLine("Console.WriteLine basically hits Enter for you...");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            //Bailey Sucks
+            Console.Write("Hello world!  ");
+            Console.Write("This is on the same line.    ");
+            Console.Write("While Conslole.Write does not");
+            Console.WriteLine();
+            Console.WriteLine("Comments are used to communicate the purpose of specific code to yourself or other programmers.");
+            Console.WriteLine();
+            Console.WriteLine("Comments can also be used to skip a section of code while troubleshooting");
+            Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
